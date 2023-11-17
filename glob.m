79e7514aef6c81d92f8f50345e418ce852ed4b2c
Will Decker <jdecke5@lsu.edu>
2023-11-17 10:23:57 -0600

    align input args for continuity

--- glob.m
+++ glob.m
@@ -1,19 +1,19 @@
-function [g] = glob(A, str)
+function [g] = glob(a, str)
 %GLOB Filter array-like object with string conditional
 %   A array-like object
 %   str String to find in the data and filter with
 
-    if isstruct(A) == true
+    if isstruct(a) == true
         error("Please provide the fieldname of %s as a cell array. \n" + ...
             "Example: {struct.fieldname}.'", char(inputname(1)))
     end
 
     g = [];
-    for i=1:length(A)
-        if contains(A{i}, str) == true
-            g{end + 1} = A{i};
+    for i=1:length(a)
+        if contains(a{i}, str) == true
+            g{end + 1} = a{i};
         else
-            disp("'" + A{i} + "'" + " does not match filter, " + ...
+            disp("'" + a{i} + "'" + " does not match filter, " + ...
                 "" + "'" + str + "'.")
         end
     end
